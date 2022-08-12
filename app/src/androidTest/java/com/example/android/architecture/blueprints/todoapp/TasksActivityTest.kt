0cc2352b23f144e18934f7280581169b686af636
yoosangyeob <syyoo@joara.com>
2022-08-12 18:00:57 +0900

    Task: End-to-End Testing with Data Binding - Use Idling Resources in tests

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/TasksActivityTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/TasksActivityTest.kt
@@ -3,6 +3,7 @@ package com.example.android.architecture.blueprints.todoapp
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
@@ -13,6 +14,9 @@ import androidx.test.filters.LargeTest
 import com.example.android.architecture.blueprints.todoapp.data.Task
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
 import com.example.android.architecture.blueprints.todoapp.tasks.TasksActivity
+import com.example.android.architecture.blueprints.todoapp.util.DataBindingIdlingResource
+import com.example.android.architecture.blueprints.todoapp.util.EspressoIdlingResource
+import com.example.android.architecture.blueprints.todoapp.util.monitorActivity
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.Matchers.not
 import org.junit.After
@@ -25,6 +29,9 @@ import org.junit.runner.RunWith
 class TasksActivityTest {
 
     private lateinit var repository: TasksRepository
+
+    // An idling resource that waits for Data Binding to have no pending bindings.
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
 
     @Before
     fun init() {
@@ -39,6 +46,25 @@ class TasksActivityTest {
         ServiceLocator.resetRepository()
     }
 
+    /**
+     * Idling resources tell Espresso that the app is idle or busy. This is needed when operations
+     * are not scheduled in the main Looper (for example when executed on a different thread).
+     */
+    @Before
+    fun registerIdlingResource() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
+
+    /**
+     * Unregister your Idling Resource so it can be garbage collected and does not leak any memory.
+     */
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
+
     @Test
     fun editTask() = runBlocking {
 
@@ -47,7 +73,7 @@ class TasksActivityTest {
 
         // Start up Tasks screen.
         val activityScenario = ActivityScenario.launch(TasksActivity::class.java)
-
+        dataBindingIdlingResource.monitorActivity(activityScenario)
         // Click on the task on the list and verify that all the data is correct.
         onView(withText("TITLE1")).perform(click())
         onView(withId(R.id.task_detail_title_text)).check(matches(withText("TITLE1")))
